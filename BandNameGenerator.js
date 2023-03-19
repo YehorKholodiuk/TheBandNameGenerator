155ac5ce19650d3161af3994b7270cd23b5edcc2
ASUS <Yehor Kholodiuk>
2023-03-19 18:33:16 +0200

    initial commit

---
+++ BandNameGenerator.js
@@ -0,0 +1,7 @@
+function bandNameGenerator(str){
+    if (str[0] == str[str.length-1]){
+    return str[0].toUpperCase() + str.slice(1) + str.slice(1)
+    }
+return `The ` + str[0].toUpperCase() + str.slice(1);
+}
+console.log(bandNameGenerator('Alison'))
